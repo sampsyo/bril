707489919e0bd0d05ecb2575f9159fde2de31b10
Adrian Sampson <adrian@radbox.org>
2019-05-14 15:10:30 -0400

    Test conditionals

---
+++ test/cond.ts
@@ -0,0 +1,6 @@
+var value = 5;
+if (value + 3 < 6) {
+  console.log(1);
+} else {
+  console.log(2);
+}
---
+++ test/cond.ts.t
@@ -0,0 +1,4 @@
+A simple program with conditionals:
+
+  $ ts2bril < $TESTDIR/cond.ts | brili
+  2
