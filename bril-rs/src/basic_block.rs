e617fec6a93bacb1235685fbca6008123ceb3490
Wil Thomason <wil.thomason@gmail.com>
2019-09-12 20:21:21 -0400

    Move to using indices instead of Rc and add compiling but untested CFG algorithm

--- bril-rs/src/basic_block.rs
+++ bril-rs/src/basic_block.rs
@@ -1,85 +1,80 @@
 use crate::ir_types::{Function, Instruction, Program};
 use std::collections::HashMap;
-use std::rc::Rc;
 
 pub struct BasicBlock {
-    instrs: Vec<Instruction>,
-    exit: Vec<Rc<BasicBlock>>,
+  pub instrs: Vec<Instruction>,
+  pub exit: Vec<usize>,
 }
 
 impl BasicBlock {
-    fn new() -> BasicBlock {
-        BasicBlock {
-            instrs: Vec::new(),
-            exit: Vec::new(),
-        }
+  fn new() -> BasicBlock {
+    BasicBlock {
+      instrs: Vec::new(),
+      exit: Vec::new(),
     }
+  }
 }
 
 pub fn find_basic_blocks(
-    prog: Program,
-) -> (
-    Option<Rc<BasicBlock>>,
-    Vec<Rc<BasicBlock>>,
-    HashMap<String, Rc<BasicBlock>>,
-) {
-    let mut main_fn = None;
-    let mut blocks = Vec::new();
-    let mut labels = HashMap::new();
+  prog: Program,
+) -> (Option<usize>, Vec<BasicBlock>, HashMap<String, usize>) {
+  let mut main_fn = None;
+  let mut blocks = Vec::new();
+  let mut labels = HashMap::new();
 
-    let mut bb_helper = |func: Function| -> Rc<BasicBlock> {
-        let mut curr_block = BasicBlock::new();
-        let root_block = None;
-        let mut label = None;
-        for instr in func.instrs.into_iter() {
-            match instr {
-                Instruction::Label(ref l) => {
-                    if !curr_block.instrs.is_empty() {
-                        blocks.push(Rc::new(curr_block));
-                        if let Some(old_label) = label {
-                            labels.insert(old_label, Rc::clone(blocks.last().unwrap()));
-                        }
-
-                        curr_block = BasicBlock::new();
-                    }
-
-                    label = Some(l.label.clone());
-                    curr_block.instrs.push(instr);
-                }
-
-                Instruction::Jmp { .. } | Instruction::Br { .. } | Instruction::Ret { .. } => {
-                    curr_block.instrs.push(instr);
-                    blocks.push(Rc::new(curr_block));
-                    if let Some(l) = label {
-                        labels.insert(l, Rc::clone(blocks.last().unwrap()));
-                        label = None;
-                    }
-
-                    curr_block = BasicBlock::new();
-                }
-                _ => {
-                    curr_block.instrs.push(instr);
-                }
+  let mut bb_helper = |func: Function| -> usize {
+    let mut curr_block = BasicBlock::new();
+    let root_block = blocks.len();
+    let mut label = None;
+    for instr in func.instrs.into_iter() {
+      match instr {
+        Instruction::Label(ref l) => {
+          if !curr_block.instrs.is_empty() {
+            blocks.push(curr_block);
+            if let Some(old_label) = label {
+              labels.insert(old_label, blocks.len() - 1);
             }
+
+            curr_block = BasicBlock::new();
+          }
+
+          label = Some(l.label.clone());
+          curr_block.instrs.push(instr);
         }
 
-        if !curr_block.instrs.is_empty() {
-            blocks.push(Rc::new(curr_block));
-            if let Some(l) = label {
-                labels.insert(l, Rc::clone(blocks.last().unwrap()));
-            }
-        }
+        Instruction::Jmp { .. } | Instruction::Br { .. } | Instruction::Ret { .. } => {
+          curr_block.instrs.push(instr);
+          blocks.push(curr_block);
+          if let Some(l) = label {
+            labels.insert(l, blocks.len() - 1);
+            label = None;
+          }
 
-        root_block.unwrap()
-    };
-
-    for func in prog.functions.into_iter() {
-        let func_name = func.name.clone();
-        let func_block = bb_helper(func);
-        if func_name == "main" {
-            main_fn = Some(func_block);
+          curr_block = BasicBlock::new();
         }
+        _ => {
+          curr_block.instrs.push(instr);
+        }
+      }
     }
 
-    (main_fn, blocks, labels)
+    if !curr_block.instrs.is_empty() {
+      blocks.push(curr_block);
+      if let Some(l) = label {
+        labels.insert(l, blocks.len() - 1);
+      }
+    }
+
+    root_block
+  };
+
+  for func in prog.functions.into_iter() {
+    let func_name = func.name.clone();
+    let func_block = bb_helper(func);
+    if func_name == "main" {
+      main_fn = Some(func_block);
+    }
+  }
+
+  (main_fn, blocks, labels)
 }
---
+++ bril-rs/src/cfg.rs
@@ -0,0 +1,35 @@
+use crate::basic_block::BasicBlock;
+use crate::ir_types::Instruction;
+
+use std::collections::HashMap;
+
+type CFG = Vec<BasicBlock>;
+
+pub fn build_cfg(mut blocks: Vec<BasicBlock>, labels: &HashMap<String, usize>) -> CFG {
+  let last_idx = blocks.len() - 1;
+  for (i, block) in blocks.iter_mut().enumerate() {
+    // If we're before the last block
+    if i < last_idx {
+      // Get the last instruction
+      let last_instr: &Instruction = block.instrs.last().unwrap();
+      match last_instr {
+        // Either the last instruction is a terminal instruction...
+        Instruction::Jmp { params } | Instruction::Br { params } => {
+          for arg in params.args.iter() {
+            block.exit.push(labels[arg]);
+          }
+        }
+
+        // Or it's a return, which we hack around until function calls are implemented
+        Instruction::Ret { .. } => {}
+
+        // Or the first instruction of the *next* block is a label and we fall through
+        _ => {
+          block.exit.push(i + 1);
+        }
+      }
+    }
+  }
+
+  blocks
+}
