ef6b742ce4301afd096afd3491b0a2c69969aeca
Wil Thomason <wil.thomason@gmail.com>
2019-09-18 14:05:00 -0400

    Make parser operate over streams, not paths

--- brilirs/src/parse.rs
+++ brilirs/src/parse.rs
@@ -1,13 +1,8 @@
 use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
 
 use crate::ir_types::Program;
 
-pub fn load(bril_file_path: &Path) -> Result<Program, Box<dyn Error>> {
-  let bril_file = File::open(bril_file_path)?;
-  let bril_data = BufReader::new(bril_file);
-  let bril_prog = serde_json::from_reader(bril_data)?;
-  Ok(bril_prog)
+pub fn load(bril_file_stream: Box<dyn std::io::Read>) -> Result<Program, Box<dyn Error>> {
+    let bril_prog = serde_json::from_reader(bril_file_stream)?;
+    Ok(bril_prog)
 }
