56ac9d6c092724159ab7aafcd23429b2979e6cc5
Sameer <sjl328@cornell.edu>
2019-09-21 16:27:54 -0400

    Revert "added throw for main function missing"
    This reverts commit 65a69fb0af5b75a11e81ee63cd67faa06fc0ddc7.

--- bril-ts/brili.ts
+++ bril-ts/brili.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import * as bril from './bril';
-import { readStdin, unreachable } from './util';
+import {readStdin, unreachable} from './util';
 
-const argCounts: { [key in bril.OpCode]: number | null } = {
+const argCounts: {[key in bril.OpCode]: number | null} = {
   add: 2,
   mul: 2,
   sub: 2,
@@ -68,11 +68,11 @@ function getBool(instr: bril.Operation, env: Env, index: number) {
  * control to a label, go to the next instruction, or end thefunction.
  */
 type Action =
-  { "label": bril.Ident } |
-  { "next": true } |
-  { "end": true };
-let NEXT: Action = { "next": true };
-let END: Action = { "end": true };
+  {"label": bril.Ident} |
+  {"next": true} |
+  {"end": true};
+let NEXT: Action = {"next": true};
+let END: Action = {"end": true};
 
 /**
  * Interpret an instruction in a given environment, possibly updating the
@@ -95,153 +95,153 @@ function evalInstr(
   }
 
   switch (instr.op) {
-    case "const":
-      // Ensure that JSON ints get represented appropriately.
-      let value: Value;
-      if (typeof instr.value === "number") {
-        value = BigInt(instr.value);
-      } else {
-        value = instr.value;
+  case "const":
+    // Ensure that JSON ints get represented appropriately.
+    let value: Value;
+    if (typeof instr.value === "number") {
+      value = BigInt(instr.value);
+    } else {
+      value = instr.value;
+    }
+
+    env.set(instr.dest, value);
+    return NEXT;
+
+  case "id": {
+    let val = get(env, instr.args[0]);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "add": {
+    let val = getInt(instr, env, 0) + getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "mul": {
+    let val = getInt(instr, env, 0) * getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "sub": {
+    let val = getInt(instr, env, 0) - getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "div": {
+    let val = getInt(instr, env, 0) / getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "le": {
+    let val = getInt(instr, env, 0) <= getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "lt": {
+    let val = getInt(instr, env, 0) < getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "gt": {
+    let val = getInt(instr, env, 0) > getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "ge": {
+    let val = getInt(instr, env, 0) >= getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "eq": {
+    let val = getInt(instr, env, 0) === getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "not": {
+    let val = !getBool(instr, env, 0);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "and": {
+    let val = getBool(instr, env, 0) && getBool(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "or": {
+    let val = getBool(instr, env, 0) || getBool(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "print": {
+    let values = instr.args.map(i => get(env, i).toString());
+    console.log(...values);
+    return NEXT;
+  }
+
+  case "jmp": {
+    return {"label": instr.args[0]};
+  }
+
+  case "br": {
+    let cond = getBool(instr, env, 0);
+    if (cond) {
+      return {"label": instr.args[1]};
+    } else {
+      return {"label": instr.args[2]};
+    }
+  }
+
+  case "call": {
+    let name = instr.args[0];
+    if (functionMap.has(name)) {
+      let newEnv = new Map();
+      let func = functionMap.get(name) as bril.Function;
+      let args = instr.args.slice(1);
+      if (func.args === undefined && args.length > 0
+        || args.length > 0 && func.args.length !== args.length) {
+        throw `function ${name} expects ${func.args.length} arguments, ` +
+        `got ${args.length}`;
       }
-
-      env.set(instr.dest, value);
-      return NEXT;
-
-    case "id": {
-      let val = get(env, instr.args[0]);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "add": {
-      let val = getInt(instr, env, 0) + getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "mul": {
-      let val = getInt(instr, env, 0) * getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "sub": {
-      let val = getInt(instr, env, 0) - getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "div": {
-      let val = getInt(instr, env, 0) / getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "le": {
-      let val = getInt(instr, env, 0) <= getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "lt": {
-      let val = getInt(instr, env, 0) < getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "gt": {
-      let val = getInt(instr, env, 0) > getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "ge": {
-      let val = getInt(instr, env, 0) >= getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "eq": {
-      let val = getInt(instr, env, 0) === getInt(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "not": {
-      let val = !getBool(instr, env, 0);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "and": {
-      let val = getBool(instr, env, 0) && getBool(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "or": {
-      let val = getBool(instr, env, 0) || getBool(instr, env, 1);
-      env.set(instr.dest, val);
-      return NEXT;
-    }
-
-    case "print": {
-      let values = instr.args.map(i => get(env, i).toString());
-      console.log(...values);
-      return NEXT;
-    }
-
-    case "jmp": {
-      return { "label": instr.args[0] };
-    }
-
-    case "br": {
-      let cond = getBool(instr, env, 0);
-      if (cond) {
-        return { "label": instr.args[1] };
-      } else {
-        return { "label": instr.args[2] };
+      for (let i = 0; i < args.length; i++) {
+        newEnv.set(func.args[i].name, env.get(args[i]));
       }
-    }
-
-    case "call": {
-      let name = instr.args[0];
-      if (functionMap.has(name)) {
-        let newEnv = new Map();
-        let func = functionMap.get(name) as bril.Function;
-        let args = instr.args.slice(1);
-        if (func.args === undefined && args.length > 0
-          || args.length > 0 && func.args.length !== args.length) {
-          throw `function ${name} expects ${func.args.length} arguments, ` +
-          `got ${args.length}`;
-        }
-        for (let i = 0; i < args.length; i++) {
-          newEnv.set(func.args[i].name, env.get(args[i]));
-        }
-        evalFunc(func, newEnv, functionMap);
-        if (func.type !== undefined) {
-          let returnVal = newEnv.get(returnVar);
-          env.set(instr.dest, returnVal);
-        } else if (instr.dest !== undefined) {
-          throw `function ${func.name} does not return`;
-        }
-      } else {
-        throw `function ${name} not found`;
+      evalFunc(func, newEnv, functionMap);
+      if (func.type !== undefined) {
+        let returnVal = newEnv.get(returnVar);
+        env.set(instr.dest, returnVal);
+      } else if (instr.dest !== undefined) {
+        throw `function ${func.name} does not return`;
       }
-      return NEXT;
+    } else {
+      throw `function ${name} not found`;
     }
+    return NEXT;
+  }
 
-    case "ret": {
-      if (instr.args.length > 0) {
-        let returnVal = env.get(instr.args[0]);
-        env.set(returnVar, returnVal as any);
-      }
-      return END;
+  case "ret": {
+    if (instr.args.length > 0) {
+      let returnVal = env.get(instr.args[0]);
+      env.set(returnVar, returnVal as any);
     }
+    return END;
+  }
 
-    case "nop": {
-      return NEXT;
-    }
+  case "nop": {
+    return NEXT;
+  }
   }
   unreachable(instr);
   throw `unhandled opcode ${(instr as any).op}`;
@@ -282,7 +282,7 @@ function evalProg(prog: bril.Program, cliArgs: (Number | Boolean)[]) {
   if (functionMap.has("main")) {
     let mainFunc = functionMap.get("main") as bril.Function;
     if (mainFunc.args === undefined) {
-      mainFunc.args = [];
+        mainFunc.args = [];
     }
     if (mainFunc.args.length !== cliArgs.length) {
       throw `main function expected ${mainFunc.args.length} arguments, ` +
@@ -290,16 +290,15 @@ function evalProg(prog: bril.Program, cliArgs: (Number | Boolean)[]) {
     }
     let env = new Map();
     for (let i = 0; i < cliArgs.length; i++) {
-      env.set(mainFunc.args[i].name, cliArgs[i]);
+        env.set(mainFunc.args[i].name, cliArgs[i]);
     }
     evalFunc(functionMap.get("main"), env, functionMap);
   } else {
-    throw `main function missing`
   }
 }
 
 function isNumeric(value: string) {
-  return /^\d+$/.test(value);
+    return /^\d+$/.test(value);
 }
 
 async function main() {
@@ -308,13 +307,13 @@ async function main() {
   let rawArgs = process.argv.slice(2);
   let cliArgs = [] as (Number | Boolean)[];
   for (let arg of rawArgs) {
-    if (isNumeric(arg)) {
-      cliArgs.push(parseInt(arg));
-    } else if (arg === "true" || arg === "false") {
-      cliArgs.push(arg === "true");
-    } else {
-      throw `Argument ${arg} is not of type int or bool; exiting.`;
-    }
+      if (isNumeric(arg)) {
+          cliArgs.push(parseInt(arg));
+      } else if (arg === "true" || arg === "false") {
+          cliArgs.push(arg === "true");
+      } else {
+          throw `Argument ${arg} is not of type int or bool; exiting.`;
+      }
   }
   evalProg(prog, cliArgs);
 }
