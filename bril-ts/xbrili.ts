3e7925bc6a29afeab381e691151fd690a9c56837
Oliver Richardson <oli@cs.cornell.edu>
2019-09-25 15:33:42 -0400

    still working on exact solver, committing to transfer to laptop

---
+++ bril-ts/xbrili.ts
@@ -0,0 +1,312 @@
+#!/usr/bin/env node
+import * as bril from './bril';
+import {readStdin, unreachable} from './util';
+//require('ts-priority-queue');
+
+const argCounts: {[key in bril.OpCode]: number | null} = {
+  add: 2,
+  mul: 2,
+  sub: 2,
+  div: 2,
+  id: 1,
+  lt: 2,
+  le: 2,
+  gt: 2,
+  ge: 2,
+  eq: 2,
+  not: 1,
+  and: 2,
+  or: 2,
+  print: null,  // Any number of arguments.
+  br: 3,
+  jmp: 1,
+  ret: 0,
+  nop: 0,
+  // PROB
+  flip: 0,
+  obv: 1
+};
+
+
+type Value = boolean | BigInt;
+type Env = Map<bril.Ident, Value>;
+type PEnv = Map<Env, number>;
+
+function get(env: Env, ident: bril.Ident) {
+  let val = env.get(ident);
+  if (typeof val === 'undefined') {
+    throw `undefined variable ${ident}`;
+  }
+  return val;
+}
+
+/**
+ * Ensure that the instruction has exactly `count` arguments,
+ * throwing an exception otherwise.
+ */
+function checkArgs(instr: bril.Operation, count: number) {
+  if (instr.args.length != count) {
+    throw `${instr.op} takes ${count} argument(s); got ${instr.args.length}`;
+  }
+}
+
+function getInt(instr: bril.Operation, env: Env, index: number) {
+  let val = get(env, instr.args[index]);
+  if (typeof val !== 'bigint') {
+    throw `${instr.op} argument ${index} must be a number`;
+  }
+  return val;
+}
+
+function getBool(instr: bril.Operation, env: Env, index: number) {
+  let val = get(env, instr.args[index]);
+  if (typeof val !== 'boolean') {
+    throw `${instr.op} argument ${index} must be a boolean`;
+  }
+  return val;
+}
+
+
+/**
+ * The thing to do after interpreting an instruction: either transfer
+ * control to a label, go to the next instruction, or end thefunction.
+ */
+type Action =
+  {"label": bril.Ident} |
+  {"next": true} |
+  {"end": true} |
+  {"restart" : true};
+let NEXT: Action = {"next": true};
+let END: Action = {"end": true};
+let RESTART: Action = {"restart": true};
+
+
+/**
+ * Interpret an instruction in a given environment, possibly updating the
+ * environment. If the instruction branches to a new label, return that label;
+ * otherwise, return "next" to indicate that we should proceed to the next
+ * instruction or "end" to terminate the function.
+ */
+function evalInstr(instr: bril.Instruction, env: Env, buffer: any[][]): Action {
+  // Check that we have the right number of arguments.
+  if (instr.op !== "const") {
+    let count = argCounts[instr.op];
+    if (count === undefined) {
+      throw "unknown opcode " + instr.op;
+    } else if (count !== null) {
+      checkArgs(instr, count);
+    }
+  }
+
+  switch (instr.op) {
+  case "const":
+    // Ensure that JSON ints get represented appropriately.
+    let value: Value;
+    if (typeof instr.value === "number") {
+      value = BigInt(instr.value);
+    } else {
+      value = instr.value;
+    }
+    env.set(instr.dest, value);
+    return NEXT;
+
+  case "id": {
+    let val = get(env, instr.args[0]);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "add": {
+    let val = getInt(instr, env, 0) + getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "mul": {
+    let val = getInt(instr, env, 0) * getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "sub": {
+    let val = getInt(instr, env, 0) - getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "div": {
+    let val = getInt(instr, env, 0) / getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "le": {
+    let val = getInt(instr, env, 0) <= getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "lt": {
+    let val = getInt(instr, env, 0) < getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "gt": {
+    let val = getInt(instr, env, 0) > getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "ge": {
+    let val = getInt(instr, env, 0) >= getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "eq": {
+    let val = getInt(instr, env, 0) === getInt(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "not": {
+    let val = !getBool(instr, env, 0);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "and": {
+    let val = getBool(instr, env, 0) && getBool(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "or": {
+    let val = getBool(instr, env, 0) || getBool(instr, env, 1);
+    env.set(instr.dest, val);
+    return NEXT;
+  }
+
+  case "print": {
+    let values = instr.args.map(i => get(env, i).toString());
+    buffer.push(values);
+    return NEXT;
+  }
+
+  case "jmp": {
+    return {"label": instr.args[0]};
+  }
+
+  case "br": {
+    let cond = getBool(instr, env, 0);
+    if (cond) {
+      return {"label": instr.args[1]};
+    } else {
+      return {"label": instr.args[2]};
+    }
+  }
+  
+  case "ret": {
+    return END;
+  }
+
+  case "nop": {
+    return NEXT;
+  }
+  
+  case "flip": {
+    env.set(instr.dest, Math.random() < 0.5);
+    return NEXT;
+  }
+  
+  case "obv": {
+    let cond = getBool(instr, env, 0);
+    if(cond) {
+      return NEXT;
+    } else {
+      throw new EvalError("that never happened");
+    }
+  }
+  }
+  unreachable(instr);
+  throw `unhandled opcode ${(instr as any).op}`;
+}
+
+type ProgPt = [BigInt, Env];
+
+function evalFunc(func: bril.Function, buffer: any[][] ) {
+  let curmass : Map<ProgPt, number> = new Map();
+  //let transition : Map< ProgPt, Map<ProgPt, number>> = new Map();
+  curmass.set([0, new Map()], 1);
+  
+  n_instrs_at_once = 1;
+  
+  while(runs.length > 0) {
+    // find run with highest mass 
+    let [[i, env], p] = curmass.entries().next();
+    curmass = new Map([...curmass].shift()); // very slow, do something else eventually
+    
+    //for (let i = 0; i < func.instrs.length; ++i) {
+    let line = func.instrs[i];
+    if ('op' in line) {
+      let action = evalInstr(line, env, buffer);
+      if ('label' in action) {
+        // Search for the label and transfer control.
+        for (i = 0; i < func.instrs.length; ++i) {
+          let sLine = func.instrs[i];
+          if ('label' in sLine && sLine.label === action.label) {
+            break;
+          }
+        }
+        if (i === func.instrs.length) {
+          throw `label ${action.label} not found`;
+        }
+      } else if ('end' in action) {
+        return;
+      } 
+    }
+    
+    curmass[[i, env]] = curmass.get([i,env], 0) + p;
+    
+    
+    //}
+  }
+
+}
+function evalProg(prog: bril.Program) {
+  let good_flag = false;
+  let buffer : any[][] = [];
+  
+  while(good_flag == false) {
+    good_flag = true;
+    buffer = [];
+    
+    for (let func of prog.functions) {
+      if (func.name === "main") {
+        try {
+          evalFunc(func, buffer);
+        } catch (e) {
+          switch (e.constructor) {
+            case EvalError: good_flag = false; 
+          }
+        }
+      }
+    }
+  }
+  
+  // print buffer
+  for(let i = 0; i < buffer.length; i++) {
+    console.log(...buffer[i]);
+  }
+}
+
+async function main() {
+  let prog = JSON.parse(await readStdin()) as bril.Program;
+  evalProg(prog);
+}
+
+// Make unhandled promise rejections terminate.
+process.on('unhandledRejection', e => { throw e });
+
+main();
