e5de7fa4a66e6d5b7f904012c12210cb182100dc
Sachille Atapattu <sa2257@cornell.edu>
2019-09-25 23:07:14 -0400

    vector works for full length

--- bril-ts/brili.ts
+++ bril-ts/brili.ts
@@ -128,7 +128,11 @@ function evalInstr(instr: bril.Instruction, env: Env): Action {
 
   case "vadd": {
     let idx = getInt(instr, env, 2);
-    for (let i = idx; i < idx + maxvl - 1; i++) {
+    let vlt = get(env, 'vl');
+    if (typeof vlt !== 'number') {
+      throw `${instr.op} vl must be a number`;
+    }
+    for (let i = idx; i < vlt; i++) {
     let val1 = getArr(env, instr.args[0], i);
     let val2 = getArr(env, instr.args[1], i);
     let res  = val1 + val2;
