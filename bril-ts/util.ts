359d36dffeb9b4d5ee43b36c830ebc0ce4afe853
Oliver Richardson <oli@cs.cornell.edu>
2019-09-26 14:41:07 -0400

    works except for cycle reweighting

--- bril-ts/util.ts
+++ bril-ts/util.ts
@@ -15,3 +15,100 @@ export function readStdin(): Promise<string> {
 export function unreachable(x: never) {
   throw "impossible case reached";
 }
+
+
+
+/**
+ * Taken from below, and modified to compile.
+ * https://stackoverflow.com/questions/29759480/how-to-customize-object-equality-for-javascript-set
+ * Map that stringifies the key objects in order to leverage
+ * the javascript native Map and preserve key uniqueness.
+ */
+export abstract class StringifyingMap<K, V> {
+    private map : Map<string, V> = new Map<string, V>();
+    private keyMap : Map<string, K> = new Map<string, K>();
+    
+    constructor( array : Array<[K,V]> )
+    constructor(exiting : StringifyingMap<K,V>)
+    constructor( arrayORexisting?: StringifyingMap<K,V> | Array<[K,V]>) {
+      if( arrayORexisting !== undefined) {
+        if(arrayORexisting instanceof StringifyingMap) {
+          this.map = arrayORexisting.map;
+          this.keyMap = arrayORexisting.keyMap;
+        } else {
+          for ( let [k,v] of arrayORexisting ) {
+            this.set(k,v);
+          }
+        }
+      }
+    }
+
+    has(key: K): boolean {
+        let keyString = this.stringifyKey(key);
+        return this.map.has(keyString);
+    }
+    get(key: K, defau?: V): V | undefined {
+        let keyString = this.stringifyKey(key);
+        return this.map.has(keyString) ? this.map.get(keyString) : defau;
+    }
+    set(key: K, value: V): StringifyingMap<K, V> {
+        let keyString = this.stringifyKey(key);
+        this.map.set(keyString, value);
+        this.keyMap.set(keyString, key);
+        return this;
+    }
+
+    /**
+     * Puts new key/value if key is absent.
+     * @param key key
+     * @param defaultValue default value factory
+     */
+    putIfAbsent(key: K, defaultValue: () => V): boolean {
+        if (!this.has(key)) {
+            let value = defaultValue();
+            this.set(key, value);
+            return true;
+        }
+        return false;
+    }
+
+    keys(): IterableIterator<K> {
+        return this.keyMap.values();
+    }
+
+    keyList(): K[] {
+        return [...this.keys()];
+    }
+
+    delete(key: K): boolean {
+        let keyString = this.stringifyKey(key);
+        let flag = this.map.delete(keyString);
+        this.keyMap.delete(keyString);
+        return flag;
+    }
+
+    clear(): void {
+        this.map.clear();
+        this.keyMap.clear();
+    }
+
+    size(): number {
+        return this.map.size;
+    }
+
+    /**
+     * Turns the `key` object to a primitive `string` for the underlying `Map`
+     * @param key key to be stringified
+     */
+    protected abstract stringifyKey(key: K): string;
+}
+
+/**
+ * taken from https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map
+ */
+// export function mapToObj<V>(m : Map<string,V>) {
+//   return Array.from(m).reduce((obj, [key, value]) => {
+//     obj[key] = value;
+//     return obj;
+//   }, { [key: string]: V });
+// };
--- bril-ts/xbrili.ts
+++ bril-ts/xbrili.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as bril from './bril';
-import {readStdin, unreachable} from './util';
+import {readStdin, unreachable, StringifyingMap} from './util';
 //require('ts-priority-queue');
 
 const argCounts: {[key in bril.OpCode]: number | null} = {
@@ -30,7 +30,11 @@ const argCounts: {[key in bril.OpCode]: number | null} = {
 
 type Value = boolean | BigInt;
 type Env = Map<bril.Ident, Value>;
-type PEnv = Map<Env, number>;
+class EnvMap<T> extends StringifyingMap<Env, T> {
+  protected stringifyKey(key : Env): string  {
+    return JSON.stringify( Array.from(key).sort() );
+  }
+}
 
 function get(env: Env, ident: bril.Ident) {
   let val = env.get(ident);
@@ -71,14 +75,25 @@ function getBool(instr: bril.Operation, env: Env, index: number) {
  * The thing to do after interpreting an instruction: either transfer
  * control to a label, go to the next instruction, or end thefunction.
  */
-type Action =
-  {"label": bril.Ident} |
-  {"next": true} |
-  {"end": true} |
-  {"restart" : true};
-let NEXT: Action = {"next": true};
-let END: Action = {"end": true};
-let RESTART: Action = {"restart": true};
+type PCAction =
+  { label : bril.Ident } |
+  { next : true } |
+  { end : true } |
+  { restart : true };
+let PC_NEXT: PCAction = {"next": true};
+let PC_END: PCAction = {"end": true};
+let PC_RESTART: PCAction = {"restart": true};
+
+type SplitAction = 
+  { det : true } | { "newenvs" : [Env, number][] };
+
+let ALONE: SplitAction = { det : true}
+
+type Action = PCAction & SplitAction
+let NEXT : Action = {...PC_NEXT,...ALONE };
+let END : Action = {...PC_END,...ALONE };
+let BYE: Action = { newenvs : [], ...PC_RESTART };
+
 
 
 /**
@@ -158,7 +173,7 @@ function evalInstr(instr: bril.Instruction, env: Env, buffer: any[][]): Action {
     return NEXT;
   }
 
-  case "ge": {
+  case "ge": {instr.dest, Math.random() < 0.5
     let val = getInt(instr, env, 0) >= getInt(instr, env, 1);
     env.set(instr.dest, val);
     return NEXT;
@@ -195,15 +210,15 @@ function evalInstr(instr: bril.Instruction, env: Env, buffer: any[][]): Action {
   }
 
   case "jmp": {
-    return {"label": instr.args[0]};
+    return {"label": instr.args[0], ...ALONE};
   }
 
   case "br": {
     let cond = getBool(instr, env, 0);
     if (cond) {
-      return {"label": instr.args[1]};
+      return {"label": instr.args[1], ...ALONE};
     } else {
-      return {"label": instr.args[2]};
+      return {"label": instr.args[2], ...ALONE};
     }
   }
   
@@ -216,41 +231,73 @@ function evalInstr(instr: bril.Instruction, env: Env, buffer: any[][]): Action {
   }
   
   case "flip": {
-    env.set(instr.dest, Math.random() < 0.5);
-    return NEXT;
+    let newE1 = new Map(env); // clone env, do both.
+    let newE2 = new Map(env); // clone env, do both.
+    newE1.set(instr.dest, true);
+    newE2.set(instr.dest, false); 
+    return { newenvs : [[newE1, 0.5], [newE2, 0.5]], ...NEXT};
   }
   
   case "obv": {
     let cond = getBool(instr, env, 0);
-    if(cond) {
-      return NEXT;
-    } else {
-      throw new EvalError("that never happened");
-    }
+    return cond ? NEXT : BYE;
   }
   }
   unreachable(instr);
   throw `unhandled opcode ${(instr as any).op}`;
 }
 
-type ProgPt = [BigInt, Env];
+type ProgPt = [number, Env]; // program counter, environment
+class Path extends StringifyingMap<ProgPt, [ProgPt | undefined, number]> {
+  protected stringifyKey(key : ProgPt): string { return pt2str(key); }
+}
+// singlely linked list going backwards in time
+type PointedPath = { head : [ProgPt, number], hist : Path }
+// PointedPath hist includes the head.
+
+function pt2str(pt : ProgPt) : string {
+  return JSON.stringify( [pt[0], Array.from(pt[1]).sort()]);
+}
+/**
+* Extend the linked list with a point
+*/
+function extend(path : PointedPath, pt : ProgPt, p : number) { 
+  let prev_head = path.head;
+  path.hist.set( pt, prev_head )
+  path.head = [pt, p * prev_head[1] /* multiply probability */ ];
+}
+function path_copy(path: PointedPath ) {
+  return { head : path.head, hist : new Path(path.hist) };
+}
 
 function evalFunc(func: bril.Function, buffer: any[][] ) {
-  let curmass : Map<ProgPt, number> = new Map();
+  let cur_pt : ProgPt = [0, new Map()];
+  let paths : PointedPath[] = [
+    { head : [cur_pt, 1], hist : new Path([[cur_pt, [undefined, 1]]]) } ];
+  
+  let probs : Map<ProgPt, number> = new Map();
+  // also, shift.  
+  let finished : Map<Env, number> = new Map();
+  let missing_prob = 0;
   //let transition : Map< ProgPt, Map<ProgPt, number>> = new Map();
-  curmass.set([0, new Map()], 1);
+
+  let n_instrs_at_once = 1;
+  let pathidx = 0;
   
-  n_instrs_at_once = 1;
-  
-  while(runs.length > 0) {
-    // find run with highest mass 
-    let [[i, env], p] = curmass.entries().next();
-    curmass = new Map([...curmass].shift()); // very slow, do something else eventually
+  while(paths.length > pathidx) {
+    // find run with highest mass     
+    let [[i, penv], pr] = paths[pathidx].head;
+    let env = new Map(penv);
+    let cpr = 1; // conditional probability of next instruction given current one.
+    //curmass = new Map([...curmass].shift()); // very slow, do something else eventually
     
     //for (let i = 0; i < func.instrs.length; ++i) {
     let line = func.instrs[i];
+    
     if ('op' in line) {
       let action = evalInstr(line, env, buffer);
+      
+      // handle motion form PCActions
       if ('label' in action) {
         // Search for the label and transfer control.
         for (i = 0; i < func.instrs.length; ++i) {
@@ -259,39 +306,78 @@ function evalFunc(func: bril.Function, buffer: any[][] ) {
             break;
           }
         }
+        
+        
         if (i === func.instrs.length) {
           throw `label ${action.label} not found`;
         }
       } else if ('end' in action) {
-        return;
-      } 
+        pathidx ++; 
+        finished.set(env, (finished.get(env) || 0) + pr)
+        continue;
+      } else if ('next' in action) {
+        i++;
+        if (i == func.instrs.length) { 
+          pathidx ++;
+          finished.set(env, (finished.get(env) || 0) + pr)
+          continue; 
+        }
+      }
+      
+      //handle world splitting and env copying
+      if ( 'newenvs' in action ) {
+        let totalp = 0;
+        for ( let [e, p] of action.newenvs) {
+          totalp += p;
+          let copy = path_copy(paths[pathidx]);
+          extend(copy, [i, e], p);
+          paths.push(copy);
+        }
+        
+        if(totalp > 1){
+          console.warn("No convergence guarantees if positive weighting can occur.")
+        } else {
+          missing_prob += pr * (1-totalp);
+        }
+        
+        pathidx ++;
+        continue;
+      }
+    } else { // this is a label. Copy environment.
+      env = new Map(env);
+      i++;
     }
     
-    curmass[[i, env]] = curmass.get([i,env], 0) + p;
-    
+    if ( paths[pathidx].hist.has([i,env]) ) {
+      let factor = 1 / (1 - pr / paths[pathidx].hist.get([i,env])![1]);
+      console.log('head: ', paths[pathidx].head[0], '[i,env]: ', [i,env],  paths[pathidx].hist.has(paths[pathidx].head[0]));
+      let [pt, pro] = paths[pathidx].hist.get(paths[pathidx].head[0])!;
+      
+      while( pt !== undefined && pt2str(pt) != pt2str([i,env]) ){
+        [pt, pro] = paths[pathidx].hist.get(pt)!;
+      }
+      
+      pathidx ++;
+      continue;
+      //console.log(`shows up in paths[${pathidx}]`, paths[pathidx].hist)
+    } else {
+      extend(paths[pathidx], [i,env], cpr);
+    }
     
     //}
   }
-
+  finished.forEach( (p,e) => finished.set(e, p / (1-missing_prob)))
+  
+  console.log('paths', paths);
+  console.log('finished');
+  finished.forEach( (p, e) =>  console.log(e, 'prob = ', p) );
 }
 function evalProg(prog: bril.Program) {
-  let good_flag = false;
   let buffer : any[][] = [];
   
-  while(good_flag == false) {
-    good_flag = true;
-    buffer = [];
-    
-    for (let func of prog.functions) {
-      if (func.name === "main") {
-        try {
-          evalFunc(func, buffer);
-        } catch (e) {
-          switch (e.constructor) {
-            case EvalError: good_flag = false; 
-          }
-        }
-      }
+  for (let func of prog.functions) {
+    if (func.name === "main") {
+      evalFunc(func, buffer);
     }
   }
   
