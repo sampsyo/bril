29cf621874335814c14bd8a824f92ba72600723b
Zhijing <173666635@qq.com>
2019-10-16 19:32:43 -0400

    loop invariant code motion

--- code/funcs.py
+++ code/funcs.py
@@ -1,13 +1,13 @@
 import json
-import sys
-
+import copy
+from collections import OrderedDict
 import sys
 sys.path.insert(0, "examples/")
-
 from cfg import block_map, successors, add_terminators
 from form_blocks import form_blocks
 from dom import get_dom, get_pred
-
+from util import fresh
+from df import df_worklist, ANALYSES
 
 def union(dicts):
     out = {}
@@ -88,7 +88,7 @@ def back_search(node,explored,pred,loop):
 
 def find_loops(back_edges,pred):
 
-    natural_loop = []
+    natural_loop = {}
     for t,h in back_edges:
         loop = []
         #find all blocks which reach t without h
@@ -97,15 +97,26 @@ def find_loops(back_edges,pred):
         loop.append(h) #adding header to loop
 
         back_search(t,explored,pred,loop)
-        natural_loop.append(loop)
+        natural_loop[(t,h)] = loop
 
     return natural_loop
 
+def find_exits(succ, loops):
+    exits = {}
+    for k in loops:
+        exits[k] = []
+        for l in loops[k]: 
+            if any( s not in loops[k] for s in succ[l]):
+                exits[k].append(l)
+    return exits
 
-
-
-def loop_king(bril):
+def loop_king(func):
     ''' This function returns:
+    dom: A dictionary that maps each block with blocks that dominating it.
+    
+    original blocks: A list of blocks that non teminator is added. This is to 
+    recover json from modified blocks
+    
     back_edges: A list of pairs of [tail,header] where the header dominates the
     tail and there is an edge from tail -> header
     
@@ -121,19 +132,109 @@ def loop_king(bril):
     in def and see if the block number(s) listed is not in natural_loops list
     for that loop.
     '''
-    for func in bril['functions']:
-        blocks = block_map(form_blocks(func['instrs']))
-        add_terminators(blocks)
-        succ = {name: successors(block[-1]) for name, block in blocks.items()}
-        pred = get_pred(succ)
-        dom = get_dom(succ, list(blocks.keys())[0])
+    #preprocessing
+    blocks = block_map(form_blocks(func['instrs']))
+    original_blocks = copy.deepcopy(blocks)
+    add_terminators(blocks)
+    
+    succ = {name: successors(block[-1]) for name, block in blocks.items()}
+    pred = get_pred(succ)
+    dom = get_dom(succ, list(blocks.keys())[0])
+    back_edges = get_backedges(succ,dom) 
+    natural_loops = find_loops(back_edges,pred)
+    reaching_in,reaching_out = reach_defs(blocks, succ, pred)
+    
+    exits = find_exits(succ, natural_loops)
+    live_var = df_worklist(blocks,ANALYSES['live'])
+    return exits, live_var, dom, original_blocks, natural_loops, reaching_in, reaching_out
 
-        back_edges = get_backedges (succ,dom) 
-        natural_loops = find_loops(back_edges,pred)
+def find_LI(blocks, loops, reach_def):
+    ''' This function returns:
+    loop_invariants: a dictionary of tuple(loop blocks), loop invariant inside the block.
+    '''
+    loop_invariants = {}
+    for loop in loops.keys():
+        loop_invariants[loop] = []  
+        for block in loops[loop]:
+            for instr in blocks[block]: 
+                if 'dest' in instr.keys():
+                    if instr['op']=='const':
+                    # constant
+                        loop_invariants[loop].append(instr['dest'])
+                    else:
+                    # the instr takes some variables and all variables
+                    # has reaching defintion outside the loop
+                    # has one reaching definition that is L.I.
+                        var = instr['args']
+                        defs = [x in loop or x in loop_invariants[loop] 
+                                and len(reach_def[block][x]) == 1
+                                for x in var]
+                        if all(defs):
+                            loop_invariants[loop].append(instr['dest'])
+    return loop_invariants
 
-        reaching_in,reaching_out = reach_defs (blocks, succ, pred)
 
-        return back_edges, natural_loops, reaching_in, reaching_out
-
+    
+def create_preheaders(blocks, loops):
+    new_blocks = OrderedDict()
+    b_names = list(blocks.keys())
+    #add pre header
+    pre_header = {}
+    for i, k in enumerate(b_names):
+        new_blocks[k] = blocks[k]
+        if i+1 < len(b_names):
+            for edge in loops:
+                if b_names[i+1] in edge[1]:
+                    name = fresh('b', new_blocks)
+                    new_blocks[name] = []
+                    pre_header = {x:name for x in loops[edge]}
+                    break
+    return pre_header, new_blocks
+    
+def move_LI(blocks, pre_header, loop_invariants, loops, dom, live_var, exits):
+    b_names = list(blocks.keys())
+    for back_edge in loops:
+        for b_name in loops[back_edge]:
+            for instr in blocks[b_name]:
+                if 'dest' in instr and instr['dest'] in loop_invariants[back_edge]:
+                    #exist block of dest where dest is live out 
+                    edest = [e for e in exits[back_edge] 
+                            if instr['dest'] in live_var[1][e]]
+                    #predecessor block of pre header
+                    ind = b_names.index(pre_header[b_name])- 1 
+                    # 1. there is only one definition of dest in the loop
+                    # 2. dest's block dominates all loop exists where dest is live out
+                    # 3. dest is not live out of pre-header
+                    if ( loop_invariants[back_edge].count(instr['dest'])==1 and
+                         all([b_name in dom[e]  for e in edest]) and
+                         ind >= 0 and instr['dest'] not in live_var[1][b_names[ind]]
+                        ):
+                        blocks[b_name].remove(instr)
+                        blocks[pre_header[b_name]].append(instr)
+    return blocks
+    
+def blocks_to_func(blocks, func):
+    new_instrs = []
+    label_name = []
+    for instr in func['instrs']:
+        if 'label' in instr: label_name.append(instr['label'])
+    for block in blocks:
+        if block in label_name:
+            new_instrs.append({'label':block})
+        new_instrs = new_instrs+blocks[block]
+    new_func = copy.deepcopy(func)
+    new_func['instrs'] = new_instrs
+    return new_func
+    
 if __name__ == '__main__':
-   loop_king(json.load(sys.stdin))
+    bril = json.load(sys.stdin)
+    for func in bril['functions']:
+        exits, live_var, dom, oblocks, loops, reach_def, _ = loop_king(func)
+        loop_invariants = find_LI(oblocks, loops, reach_def)
+        pre_header, new_blocks = create_preheaders(oblocks, loops)
+        code_motion = move_LI(new_blocks, pre_header, loop_invariants, loops, dom, live_var, exits)
+        blocks_to_func(code_motion, func)
+   
+   
+   
+   
