a6ba3c90f6900aa8fc4e2786daf3183a45221bcb
Henry Liu <liuhenry4428@gmail.com>
2019-10-20 18:58:19 -0400

    Everything works

--- examples/backedge.py
+++ examples/backedge.py
@@ -36,11 +36,11 @@ def findLoops(res, blocks):
         loops.append(path)
     return loops
 
-def findInitialInductionVarialbeValue(out_cprop,blocks, header, last, iv):
+def findInitialIVValue(out_cprop,blocks, loop, iv):
     preds, succs = edges(blocks)
-    preds = preds[header]
-    for pred in preds:
-        if (pred == last):
+    cond_var_preds = preds[loop[0]]
+    for pred in cond_var_preds:
+        if (pred in loop):
             continue
         val = cprop_merge([out_cprop[pred]])
     if (val[iv] == '?'):
@@ -51,20 +51,38 @@ def findInitialInductionVarialbeValue(out_cprop,blocks, header, last, iv):
 # def findCondDef(loop, br_cond)
 #     #iterate over loop and find the shit
 
-def findDelta(iv, out_header_rd, out_last_block_rd, out_last_block_cprop):
-    delta = out_last_block_rd[iv]
-    var = next(a for a in delta["args"] if a != iv)
-    var_val = out_last_block_cprop[var]
-    return delta["op"], var_val
+def findDelta(iv, br_cond_var_stmt_blockname, loop, out_rd, out_cprop):
+    index_of_cond_var_block = loop.index(br_cond_var_stmt_blockname)
+    index_of_block_before_cond_var_block = len(loop) - 1 if index_of_cond_var_block == 0 else index_of_cond_var_block - 1
+    name_of_block_before_cond_var_block = loop[index_of_block_before_cond_var_block]
 
+    delta_stmt = out_rd[name_of_block_before_cond_var_block][iv]
+    assert(delta_stmt is not None)
+    delta_var_name = next(a for a in delta_stmt["args"] if a != iv)
+    delta_var_val = out_cprop[name_of_block_before_cond_var_block][delta_var_name]
+    return delta_stmt["op"], delta_var_val
+
+
+
+#Asserts strictly 1 branch stmt in entire loop
 def findBranchStmt(blocks, loop):
-    foundBranch = None
+    foundBranchStmt = None
+    foundBranchBlockname = None
     for blockName in loop:
         for stmt in blocks[blockName]:
             if 'op' in stmt and stmt['op'] == 'br':
-                assert(foundBranch is not None)
-                findBranch = stmt
-    return foundBranch
+                assert(foundBranchStmt is None)
+                foundBranchStmt = stmt
+                foundBranchBlockname = blockName
+
+    return foundBranchStmt, foundBranchBlockname
+
+def findNameOfBlockWithStatement(blocks, targetStmt):
+    for name, block in blocks.items():
+        for stmt in block:
+            if stmt is targetStmt:
+                return name
+    assert("Henry" == "Has Messed Up")
 
 def findLoopInfo(bril, loops):
     blocks, in_cprop, out_cprop = run_df_return(bril, ANALYSES['cprop'])
@@ -72,40 +90,34 @@ def findLoopInfo(bril, loops):
     valid_op = ["eq", "lt", "gt", "ge", "le"]
 
     for loop in loops:
-        block = blocks[loop[0]]
-        stmt = block[-1]
-        out_header = out_rd[loop[0]]
-        out_cprop_header = out_cprop[loop[0]]
-        in_header = in_rd[loop[0]]
-        out_last_block = out_rd[loop[-1]]
-        assert stmt['op'] == 'br'
-        # print(stmt)
-        br_cond_var = stmt["args"][0]
-        br_cond = out_rd[loop[0]]
-        br_cond = br_cond[br_cond_var]
+        branch_stmt, branch_blockname = findBranchStmt(blocks, loop)
+        
+        assert branch_stmt['op'] == 'br'
+        br_cond_var = branch_stmt["args"][0]
+        br_cond_var_stmt = out_rd[branch_blockname][br_cond_var]
+        br_cond_var_stmt_blockname = findNameOfBlockWithStatement(blocks, br_cond_var_stmt)
         # cond op
-        op = br_cond['op']
-        if br_cond is None or 'op' not in br_cond or op not in valid_op:
-            continue 
-        iv_or_bound = br_cond['args']
-        l = iv_or_bound[0]
-        r = iv_or_bound[1]
-        # print("header name", loop[0])
-        # print("left value:", l)
-        # print("right value:", r)
-        # print("reaching defs from header for l", in_header[l])
-        # print("reaching defs from header for r", in_header[r])
-        l_const = in_header[l] is not None and in_header[l]['op'] == "const" and in_header[r] is None
-        r_const = in_header[r] is not None and in_header[r]["op"] == "const" and in_header[l] is None
-        # print("lconst" , l_const)
-        # print("rconst" , r_const)
-        assert l_const != r_const
-        boundvar = iv_or_bound[0] if l_const else iv_or_bound[1]
-        iv = iv_or_bound[0] if r_const else iv_or_bound[1]
-        delta_op, val= findDelta(iv, out_header, out_last_block, out_cprop[loop[-1]])
-        bound_val = out_cprop_header[boundvar]
-        iv_val = findInitialInductionVarialbeValue(out_cprop, blocks, loop[0], loop[-1], iv)
-        return iv, iv_val, boundvar, bound_val, op, delta_op, val
+        br_cond_var_stmt_op = br_cond_var_stmt['op']
+        assert(br_cond_var_stmt_op in valid_op)
+
+        iv_and_bound = br_cond_var_stmt['args']
+        l = iv_and_bound[0]
+        r = iv_and_bound[1]
+        l_cprop = in_cprop[br_cond_var_stmt_blockname][l]
+        r_cprop = in_cprop[br_cond_var_stmt_blockname][r]
+        assert((l_cprop != '?') != (r_cprop != '?'))
+
+        br_cond_block_in_rd = in_rd[br_cond_var_stmt_blockname] #reaching defs in edge of the condition statement's block
+        bound_var = l if l_cprop != '?' else r
+        iv = l if r_cprop != '?' else r
+
+        iv_stmt = br_cond_block_in_rd[iv]
+
+        delta_op, delta_var_val= findDelta(iv, br_cond_var_stmt_blockname, loop, out_rd, out_cprop)
+        bound_val = in_cprop[br_cond_var_stmt_blockname][bound_var]
+        iv_val = findInitialIVValue(out_cprop,blocks, loop, iv)
+        assert(iv_val is not None)
+        return iv, iv_val, bound_var, bound_val, br_cond_var_stmt_op, delta_op, delta_var_val
 
 
 
@@ -113,11 +125,9 @@ if __name__ == '__main__':
     bril = json.load(sys.stdin)
     res, blocks = backedge(bril)
     loops = findLoops(res, blocks)
-    print("loops@@@@@@", loops)
-    for item in loops[0]:
-        for item1 in blocks[item]:
-            print(item1)
-    print("finding loop info")
+    # for item in loops[0]:
+    #     for item1 in blocks[item]:
+    #         print(item1)
     a, b, c, d, e, f, g = findLoopInfo(bril, loops)
     print (a,b,c,d,e,f,g)
 
