27ef4e808b068d37ddb98eb1d5d875142b0427b4
Rolph Recto <rolph.recto@gmail.com>
2019-10-20 02:15:11 -0400

    fixed merge

--- examples/loop.py
+++ examples/loop.py
@@ -36,18 +36,11 @@ def loopsy(source,sink,predecessors):
     return loop
 
 
-### find all the natural loops
-def natloops(blocks):
-  pred,succ = edges(blocks)
-  dom = get_dom(succ,list(blocks.keys())[0])
-  for source,sink in get_backedges(succ,dom):
-    yield loopsy(source,sink,pred)
-
-
 ### apply reaching definitions analysis
 def reachers(blocks):
   rins, routs = df_worklist(blocks, ANALYSIS["rdef"])
   return rins,routs
+
 
 ### get variable information for reaching definitions
 def reaching_def_vars(blocks, reaching_defs):
@@ -64,82 +57,6 @@ def reaching_def_vars(blocks, reaching_defs):
 
   return rdef_vars
 
-### detect LI instructions for a single natural loop
-def invloop(blocks,rdef_var_ins,rdef_var_outs,natloop):
-    boolmap = {}
-    worklist = []
-    for blockname in natloop:
-        boolmap[blockname] = [False for i in range(len(blocks[blockname]))]
-        worklist.append(blockname)
-
-    while len(worklist)>0:
-      blockname = worklist.pop()
-      block = blocks[blockname]
-
-      boolmap_block = []
-      for instr in block:
-        # assignment of a constant to a variable
-        if "dest" in instr and "value" in instr:
-          boolmap_block.append(True)
-
-        # assignment of a computation to a variable
-        elif "dest" in instr and "args" in instr:
-          # for each argument, either one of the following has to be true:
-          # * all reaching defs of the argument are outside the loop
-          # * there is exactly one reaching def for the argument in the loop
-          instr_loop_invariant = True
-          for arg in instr["args"]:
-            var = instr["dest"]
-            var_rdefs = list(filter(lambda rdef: rdef[2] == var, \
-                rdef_var_ins[blockname]))
-          
-            var_rdefs_blocks = \
-                map(lambda rdef: rdef[0] not in natloop, var_rdefs)
-
-            rdefs_outside = all(var_rdefs_blocks)
-
-            single_rdef = False
-            if len(var_rdefs) == 1:
-              rdef_block, rdef_instr, _ = var_rdefs[0]
-              if rdef_block in natloop:
-                single_rdef = boolmap[rdef_block][rdef_instr]
-
-            instr_loop_invariant = \
-                instr_loop_invariant and (rdefs_outside or single_rdef)
-
-          boolmap_block.append(instr_loop_invariant)
-
-        else:
-          boolmap_block.append(False)
-      
-      boolmap[blockname] = boolmap_block
-
-
-    # is it loop invariant 
-    # does it dominate all uses
-    # no other definitions of same variable
-    # dominates all loop exits
-    return boolmap
-
-### move stuff
-def codemot(bril):
-  for func in bril['functions']:
-    blocks = block_map(form_blocks(func['instrs']))
-    add_terminators(blocks)
-    rins, routs = reachers(blocks) # what is reaching
-    rdef_var_ins  = reaching_def_vars(blocks, rins)
-    rdef_var_outs = reaching_def_vars(blocks, rins)
-
-    for natloop in natloops(blocks):
-      boolmap = invloop(blocks,rdef_var_ins,rdef_var_outs,natloop) 
-
-def natloops(blocks): #input backedge
-  pred,succ = edges(blocks)
-  dom = get_dom(succ,list(blocks.keys())[0])
-  for source,sink in get_backedges(succ,dom):
-    yield loopsy(source,sink,pred) # natloops
-
-def printstuffs(bril):
 
 # detect loop-invariant instructions for a single natural loop
 def invloop(blocks,rdef_var_ins,rdef_var_outs,natloop):
