86e1053cce6f49509768ac12c98eb72a80fdba77
Zhijing Li <zl679@cornell.edu>
2019-10-22 19:23:23 -0400

    Update codemotion2.ts

--- test/loop/codemotion2.ts
+++ test/loop/codemotion2.ts
@@ -3,7 +3,7 @@ let result = 1;
 for (let i = value; i > 0; i = i - 1) {
   for (let j = value; j > i; j = j - 1){
     if (j > 5) {
-      result = result * i;
+      result = result + i * i;
     } else {
       result = result * j;
     }
