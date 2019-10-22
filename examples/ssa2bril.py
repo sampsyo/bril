04d0dc3227283e513244e20c8adaf64fd67ab25a
Alexa VanHattum <amv94@cornell.edu>
2019-10-21 22:35:24 -0400

    Add correct type to phi nodes

--- examples/ssa2bril.py
+++ examples/ssa2bril.py
@@ -11,12 +11,39 @@ from dom_frontier import get_frontiers
 from form_blocks import form_blocks
 from util import fresh
 
+
+def replace_phis_with_copies(blocks):
+    for name, instrs in blocks.items():
+        num_phis = 0
+        for instr in instrs:
+            if instr['op'] != 'phi': break
+
+            for arg, pred_name in zip(instr['args'], instr['sources']):
+                pred_instrs = blocks[pred_name]
+
+                copy_block = fresh('copy', blocks)
+                # copy_instr = {
+                #     'op' : 'id',
+                #     'dest' : 
+                # }
+
+                terminator = pred_instrs[-1]['args']
+
+
+                pred_instrs[-1]['args'] = [a if ]
+
+
+            num_phis += 1
+           
+
 def from_ssa(bril): 
     for func in bril['functions']:
         blocks = block_map(form_blocks(func['instrs']))
         add_terminators(blocks)
         succ = {name: block_successors(block) for name, block in blocks.items()}
-        
+
+
+
 
 def ssa2bril():
     bril_without_phis = from_ssa(json.load(sys.stdin))
