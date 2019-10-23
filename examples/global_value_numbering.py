ce5930346a709bded9a3b697904ed80521281dde
Gregory Yauney <gyauney@media.mit.edu>
2019-10-23 13:12:58 -0400

    add global value numbering skeleton

--- examples/global_value_numbering.py
+++ examples/global_value_numbering.py
@@ -9,6 +9,42 @@ from dom_frontier import get_dominator_tree
 from form_blocks import form_blocks
 from util import block_map_to_instrs
 
+# from Briggs, Cooper, and Simpson, 1997.
+def dominator_value_numbering(block_name, blocks, succ, dom_tree):
+    # TODO intialize hash table scope
+
+    for instr in blocks[block_name]:
+        # Iterate only through phi nodes, which have been inserted at the
+        # start of the block if they exist
+        if instr['op'] != 'phi': break
+        # TODO
+        # meaningless?
+        # redundant?
+        pass
+
+    # loop through all assignments 
+    for instr in blocks[block_name]:
+        # Skip phi nodes because we just processed them
+        if 'dest' in instr and instr['op'] != 'phi': 
+            # TODO
+            pass
+
+    # Iterate through all successor blocks' phi nodes
+    for succ_name in succ[block_name]:
+        for instr in blocks[succ_name]:
+            if instr['op'] != 'phi': break
+            # Replace phi arguments if they were calculated in this block
+            for i, arg in enumerate(instr['args']):
+                # TODO
+                pass
+
+    for child_name in dom_tree[block_name]:
+        dominator_value_numbering(child_name, blocks, succ, dom_tree)
+
+    # TODO clean up hash table for this scope
+
+
+
 def global_value_numbering(bril): 
     for func in bril['functions']:
         blocks = block_map(form_blocks(func['instrs']))
@@ -19,11 +55,14 @@ def global_value_numbering(bril):
 
         func['instrs'] = block_map_to_instrs(blocks)
 
+        dominator_value_numbering('next(iter(blocks))', blocks, succ, dom_tree)
+
+
     return json.dumps(bril, indent=4)
 
 def gvn():
     bril = global_value_numbering(json.load(sys.stdin))
-    print(bril)
+    #print(bril)
 
 if __name__ == '__main__':
     gvn()
