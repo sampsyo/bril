038a2958ac699023486278d620088c0b5f8c9e06
yi jiang <elainejy@outlook.com>
2019-10-30 10:17:58 -0400

    strength

--- code/test2.ts
+++
@@ -1,6 +0,0 @@
-let a = 8;
-let n = 10;
-for (let i = n; i > 0; i = i - 1) {
-    a = a*64;
-}
-console.log(a);
--- code/test3.ts
+++
@@ -1,6 +0,0 @@
-let a = 4096;
-let n = 10;
-for (let i = n; i > 0; i = i - 1) {
-    a = a/8;
-}
-console.log(a);
