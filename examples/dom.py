9ab7fec25806763d3c1fbbc42331f8abcf4856eb
cgyurgyik <gyurgyikcp@gmail.com>
2021-01-02 20:52:21 -0500

    Add dom

--- examples/dom.py
+++ examples/dom.py
@@ -85,7 +85,7 @@ def dom_fronts(dom, succ):
         # You're in the frontier if you're not strictly dominated by the
         # current block.
         frontiers[block] = [b for b in dominated_succs
-                            if b not in dom_inv[block] or b == block]
+                            if b not in dom_inv[block] and b != block]
 
     return frontiers
 
