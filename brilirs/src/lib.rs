3518e5249dbc08eafc41b2dd01e504a49ea912cf
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2021-01-04 01:25:37 -0500

    Trimming dependencies

--- brilirs/src/lib.rs
+++ brilirs/src/lib.rs
@@ -1,23 +1,14 @@
-pub use interp::InterpError;
+use bril_rs;
 
 mod basic_block;
 mod interp;
-
-#[macro_use]
-extern crate log;
-extern crate simplelog;
-
-extern crate bril_rs;
-extern crate serde;
-extern crate serde_derive;
-extern crate serde_json;
 
 pub fn run_input<T: std::io::Write>(input: Box<dyn std::io::Read>, out: T, input_args: Vec<&str>) {
   let prog = bril_rs::load_program_from_read(input);
   let bbprog = basic_block::BBProgram::new(prog);
 
   if let Err(e) = interp::execute_main(bbprog, out, input_args) {
-    error!("{:?}", e);
+    eprintln!("{:?}", e);
     std::process::exit(2)
   };
 }
--- brilirs/src/main.rs
+++ brilirs/src/main.rs
@@ -1,49 +1,24 @@
-#[macro_use]
-extern crate clap;
-
-#[macro_use]
-extern crate log;
-extern crate simplelog;
-
-extern crate twox_hash;
-
+use clap::clap_app;
 use std::fs::File;
-
-use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 
 fn main() {
   let args = clap_app!(brilirs =>
     (version: "0.1")
     (author: "Wil Thomason <wbthomason@cs.cornell.edu>")
     (about: "An interpreter for Bril")
-    (@arg verbose: -v --verbose "Print debug information")
     (@arg FILE: -f --file "The Bril file to run. stdin is assumed if FILE is not provided")
     (@arg args: ... "Arguments for the main function ")
   )
   .get_matches();
 
-  // Super default log setup
-  TermLogger::init(
-    if args.is_present("verbose") {
-      LevelFilter::Debug
-    } else {
-      LevelFilter::Info
-    },
-    Config::default(),
-    TerminalMode::Mixed,
-  )
-  .unwrap();
-
   let input_args = args.values_of("args").unwrap_or_default().collect();
 
   let input: Box<dyn std::io::Read> = match args.value_of("FILE") {
     None => {
-      debug!("Reading from stdin");
       Box::new(std::io::stdin())
     }
 
     Some(input_file) => {
-      debug!("Reading from {}", input_file);
       Box::new(File::open(input_file).unwrap())
     }
   };
