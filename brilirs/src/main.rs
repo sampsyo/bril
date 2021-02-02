f62be2c383297e98fa8354a46173a4d9ac4c15d3
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2021-02-02 14:52:23 -0500

    Fix cli issue with negative inputs

--- brilirs/src/main.rs
+++ brilirs/src/main.rs
@@ -1,4 +1,4 @@
-use clap::clap_app;
+use clap::{clap_app, AppSettings};
 use std::fs::File;
 
 fn main() {
@@ -8,8 +8,8 @@ fn main() {
     (about: "An interpreter for Bril")
     (@arg profiling: -p "Flag to output the total number of dynamic instructions")
     (@arg FILE: -f --file +takes_value "The Bril file to run. stdin is assumed if FILE is not provided")
-    (@arg args: +allow_hyphen_values ... "Arguments for the main function")
-  )
+    (@arg args: ... "Arguments for the main function")
+  ).setting(AppSettings::AllowLeadingHyphen)
   .get_matches();
 
   let input_args = args.values_of("args").unwrap_or_default().collect();
