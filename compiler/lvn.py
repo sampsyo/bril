778fcdaed237164291293cbe78de9a5c3e5b5e9b
Aaron Tucker <aarondtucker@gmail.com>
2022-02-09 11:17:02 -0500

    refactor to make the variable lookup table an object rather than a bunch of functions that interact with each other a bunch. This refactor makes it look like a lot of things depend on the table, which is true, but makes me think that I could separate out responsibilities better

--- compiler/lvn.py
+++ compiler/lvn.py
@@ -3,37 +3,70 @@ import sys
 from collections import defaultdict
 
 COMMUTATIVE = {'add', 'mul'}
-var_to_idx = dict()
-value_to_idx = dict()
-table = [] # idx is #, then contains tuples of (value, home)
-lvn_vars = 0
 
 
-def insert_var(var, value):
-    # If it's not already in the table, add it
-    if value not in value_to_idx:
-        idx = len(table)
-        table.append((value, var))
-        value_to_idx[value] = idx
+class VarMapping:
+    def __init__(self):
+        self.var_to_idx = dict()
+        self.value_to_idx = dict()
+        self.table = [] # idx is #, then contains tuples of (value, home)
+        self.lvn_vars = 0
 
-    # always map the variable to the index
-    var_to_idx[var] = value_to_idx[value]
+    def insert_var(self, var, value):
+        # If it's not already in the table, add it
+        if value not in self.value_to_idx:
+            idx = len(self.table)
+            self.table.append((value, var))
+            self.value_to_idx[value] = idx
 
+        # always map the variable to the index
+        self.var_to_idx[var] = self.value_to_idx[value]
 
-def idx_to_home_var(idx):
-    value = table[idx][0]
-    if value[0] == 'id':
-        assert len(value) == 2
-        return idx_to_home_var(value[1])
-    return table[idx][1]
+    def var_to_idx(self, var):
+        # This gets set for every variable, and is remapped on reassignment
+        return self.var_to_idx[var]
 
+    def value_to_idx(self, value):
+        # This only gets set the first time we insert, so this code is correct
+        return self.value_to_idx[value]
 
-def idx_to_value(idx):
-    value = table[idx][0]
-    if value[0] == 'id':
-        assert len(value) == 2
-        return idx_to_value(value[1])
-    return value
+    def idx_to_home_var(self, idx):
+        # This needs to be unpacked to go through ids
+        value = self.table[idx][0]
+        if value[0] == 'id':
+            assert len(value) == 2
+            return self.idx_to_home_var(value[1])
+        return self.table[idx][1]
+
+    def idx_to_value(self, idx):
+        # This need to be unpacked to go through ids
+        value = self.table[idx][0]
+        if value[0] == 'id':
+            assert len(value) == 2
+            return self.idx_to_value(value[1])
+        return value
+
+    def unroll_ids(self, value):
+        print(value, file=sys.stderr)
+        if value[0] == 'id':
+            assert len(value) == 2
+            return self.unroll_ids(self.idx_to_value(value[1]))
+        return value
+
+    def maybe_rename_dest_store_old(self, var_counts, dest, value):
+        var_counts[dest] -= 1
+        if var_counts[dest] > 0:
+            # Insert old value so we know what it was when we try to use it
+            self.insert_var(dest, value)
+            dest = f'lvn.{self.lvn_vars}'
+            self.lvn_vars += 1
+            assert dest not in var_counts, f"Alas, {dest} is not a unique name"
+        return dest
+
+    def new_cache(self):
+        new = VarMapping()
+        new.lvn_vars = self.lvn_vars
+        return new
 
 
 def count_variables(func):
@@ -44,7 +77,28 @@ def count_variables(func):
     return ans
 
 
-def make_value(instr):
+def make_lookup(var_table, dest, type, value):
+    if value[0] == 'const':
+        return {
+            'dest': dest,
+            'op': 'const',
+            'value': value[1],
+            'type': type
+        }
+    else:
+        if value[0] == 'id':
+            idx = value[1]
+        else:
+            idx = var_table.value_to_idx[value]
+        return {
+            'dest': dest,
+            'op': 'id',
+            'args': [var_table.idx_to_home_var(idx)],
+            'type': type
+        }
+
+
+def make_value(var_table, instr):
     op = instr['op']
 
     # Replace every arg with its variable index
@@ -54,9 +108,10 @@ def make_value(instr):
             # We can actually refer to an argument that hasn't been defined
             # (like a function arg, or after a jump), so if it's not there
             # then add it.
-            if arg not in var_to_idx:
-                insert_var(arg, ("arg", arg))
-            args.append(var_to_idx[arg])
+            if arg not in var_table.var_to_idx:
+                #assert arg is str, f"tried to put invalid {arg} as argument"
+                var_table.insert_var(arg, ("arg", arg))
+            args.append(var_table.var_to_idx[arg])
     elif 'value' in instr:
         args = [instr['value']]
     else:
@@ -66,50 +121,18 @@ def make_value(instr):
     if op in COMMUTATIVE:
         args = sorted(args)
 
-    return op, *args
-
-
-def maybe_rename_dest_store_old(var_counts, dest, value):
-    var_counts[dest] -= 1
-    if var_counts[dest] > 0:
-        # Insert old value so we know what it was when we try to use it
-        insert_var(dest, value)
-        global lvn_vars
-        dest = f'lvn.{lvn_vars}'
-        lvn_vars += 1
-        assert dest not in var_counts, \
-            f"Alas, {dest} is not a unique variable name"
-    return dest
-
-
-def make_lookup(dest, type, value):
-    if value[0] == 'const':
-        return {
-            'dest': dest,
-            'op': 'const',
-            'value': value[1],
-            'type': type
-        }
-    else:
-        return {
-            'dest': dest,
-            'op': 'id',
-            'args': [idx_to_home_var(value_to_idx[value])],
-            'type': type
-        }
+    value = op, *args
+    return value
 
 
 def do_lvn():
-    global var_to_idx
-    global value_to_idx
-    global table
-
     prog = json.load(sys.stdin)
     # does this happen within a function or across a program?
     for func in prog['functions']:
+        var_table = VarMapping()
         if 'args' in func:
             for arg in func['args']:
-                insert_var(arg['name'], ('arg', arg['name']))
+                var_table.insert_var(arg['name'], ('arg', arg['name']))
 
         var_counts = count_variables(func)
         new_instrs = []
@@ -119,37 +142,41 @@ def do_lvn():
                    f"Unrecognized instruction key {key}, code might not work..."
 
             if 'op' in instr and instr['op'] not in {'jmp', 'br'}:
-                value = make_value(instr)
+                value = make_value(var_table, instr)
 
                 if 'dest' in instr:
                     dest = instr['dest']
 
                     # If the value is already there, replace instruction with lookup
-                    if value in value_to_idx:
-                        instr = make_lookup(dest, instr['type'], value)
-                        value = make_value(instr)
-                        print(instr, file=sys.stderr)
+                    if value in var_table.value_to_idx or instr['op'] == 'id':
+                        old_instr = instr
+                        value = var_table.unroll_ids(value)
+                        instr = make_lookup(var_table, dest, instr['type'], value)
+                        value = make_value(var_table, instr)
+                        print(f"replacement {old_instr} -> {instr} -> {value}", file=sys.stderr)
 
                     # If variable is going to be overwritten again...
-                    dest = maybe_rename_dest_store_old(var_counts, dest, value)
+                    dest = var_table.maybe_rename_dest_store_old(var_counts, dest, value)
 
                     # Put the value in the destination
-                    insert_var(dest, value)
+                    var_table.insert_var(dest, value)
                     instr['dest'] = dest
 
                 if 'args' in instr:
-                    new_args = [idx_to_home_var(idx) for idx in value[1:]]
+                    new_args = [var_table.idx_to_home_var(idx) for idx in value[1:]]
                     # Replace arg indices with their home variables
                     instr['args'] = new_args
                 elif 'value' in instr:
                     instr['value'] = value[1]
                 new_instrs.append(instr)
 
-            else: # if its not an op, then just put it back unchanged
-                # If it's a label instruction, then we don't actually know what
-                # the variables are any more
+            else:
+                # If it's a label instruction, then we might have jumped and
+                # so we need to drop the cache
                 if 'label' in instr:
-                    var_to_idx = dict()
+                    var_table = var_table.new_cache()
+
+                # if its not an op, then just put it back unchanged
                 new_instrs.append(instr)
 
         func['instrs'] = new_instrs
