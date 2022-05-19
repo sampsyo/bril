e0444edd72d219f06db3ee941139b7f5ff04aad9
Adrian Sampson <adrian@radbox.org>
2022-05-19 14:06:27 -0400

    Bootstrap Cranelift function signature

--- brilift/src/main.rs
+++ brilift/src/main.rs
@@ -1,10 +1,38 @@
-use bril_rs::load_abstract_program;
+use bril_rs as bril;
+use cranelift::frontend::{FunctionBuilder, FunctionBuilderContext, Variable};
+use cranelift::codegen::{ir, isa};
+
+fn compile_type(typ: bril::Type) -> ir::Type {
+    match typ {
+        bril::Type::Int => ir::types::I32,
+        bril::Type::Bool => ir::types::B1,
+    }
+}
+
+fn compile_func(func: bril::Function) {
+    // Build function signature.
+    let mut sig = ir::Signature::new(isa::CallConv::SystemV);
+    if let Some(ret) = func.return_type {
+        sig.returns.push(ir::AbiParam::new(compile_type(ret)));
+    }
+    for arg in func.args {
+        sig.params.push(ir::AbiParam::new(compile_type(arg.arg_type)));
+    }
+    
+    dbg!(sig);
+
+    for inst in func.instrs {
+        print!("{}\n", inst);
+    }
+}
 
 fn main() {
-    let prog = load_abstract_program();
+    // Load the Bril program from stdin.
+    let prog = bril::load_program();
+    
+    // Cranelift builder context.
+
     for func in prog.functions {
-        for inst in func.instrs {
-            print!("{}\n", inst);
-        }
+        compile_func(func);
     }
 }
