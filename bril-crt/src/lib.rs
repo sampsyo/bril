aa364bb62e66b5d24bb5811ed2c521318df19279
Adrian Sampson <adrian@radbox.org>
2022-05-22 11:15:38 -0400

    Remove old runtimes

--- bril-crt/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#[no_mangle]
-pub extern "C" fn print_int(i: i64) {
-    println!("{}", i);
-}
--- brilift/rt.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
-
-void print_int(int64_t i) {
-    printf("%" PRId64 "\n", i);
-}
