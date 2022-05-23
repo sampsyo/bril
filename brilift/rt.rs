674223bc7648a492139df168d954531b1ebbe78a
Adrian Sampson <adrian@radbox.org>
2022-05-23 14:21:08 -0400

    Try writing a parse function in the runtime

--- brilift/rt.rs
+++ brilift/rt.rs
@@ -2,12 +2,22 @@
 
 extern {
     pub fn printf(format: *const u8, ...) -> i32;
+    pub fn sscanf(s: *const u8, format: *const u8, ...) -> i32;
 }
 
 #[no_mangle]
 pub extern "C" fn print_int(i: i64) {
     unsafe {
         printf("%ld\n\0".as_ptr() as *const _, i);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn parse_int(argv: *const *const u8, argi: isize) -> i64 {
+    unsafe {
+        let mut res: i64 = 0;
+        sscanf(*argv.offset(argi), "%ld\0".as_ptr() as *const _, &mut res as *mut i64);
+        res
     }
 }
 
