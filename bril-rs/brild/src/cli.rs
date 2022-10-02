6c308b7b5aa099107778117b11e83091e7868996
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2022-10-02 19:35:44 -0400

    New clippy lints and Clap 4.0

--- bril-rs/brild/src/cli.rs
+++ bril-rs/brild/src/cli.rs
@@ -2,12 +2,12 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[clap(about, version, author)] // keeps the cli synced with Cargo.toml
+#[command(about, version, author)] // keeps the cli synced with Cargo.toml
 pub struct Cli {
     /// The bril file to statically link. stdin is assumed if file is not provided.
-    #[clap(short, long, action)]
+    #[arg(short, long, action)]
     pub file: Option<String>,
     /// A list of library paths to look for Bril files.
-    #[clap(short, long, action, multiple_values = true)]
+    #[arg(short, long, action, num_args=1..)]
     pub libs: Vec<PathBuf>,
 }
--- brilirs/src/basic_block.rs
+++ brilirs/src/basic_block.rs
@@ -97,14 +97,15 @@ fn get_num_from_map(
   // A map from variables to numbers
   num_var_map: &mut FxHashMap<String, usize>,
 ) -> usize {
-  match num_var_map.get(variable_name) {
-    Some(i) => *i,
-    None => {
-      let x = *num_of_vars;
-      num_var_map.insert(variable_name.to_string(), x);
-      *num_of_vars += 1;
-      x
-    }
+  // https://github.com/rust-lang/rust-clippy/issues/8346
+  #[allow(clippy::option_if_let_else)]
+  if let Some(i) = num_var_map.get(variable_name) {
+    *i
+  } else {
+    let x = *num_of_vars;
+    num_var_map.insert(variable_name.to_string(), x);
+    *num_of_vars += 1;
+    x
   }
 }
 
