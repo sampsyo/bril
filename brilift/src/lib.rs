f614884888c9fb13bce849813f21c623d56b286a
Alex Fischman <alexdfischman@gmail.com>
2024-03-14 12:37:38 -0700

    Implement lib entry point

---
+++ brilift/src/lib.rs
@@ -0,0 +1,19 @@
+mod translator;
+mod rt;
+
+use cranelift_object::ObjectModule;
+use bril_rs::program::Program;
+use crate::translator::{find_func, Translator};
+
+pub fn compile(program: Program, target: Option<String>, opt_level: &str, output: &str) {
+    // Compile.
+    let mut trans = Translator::<ObjectModule>::new(target, opt_level);
+    trans.compile_prog(&program, false);
+
+    // Add a C-style `main` wrapper.
+    let main = find_func(&program.functions, "main");
+    trans.add_c_main(&main.args, false);
+
+    // Write object file.
+    trans.emit(&output);
+}
