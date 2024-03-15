e3faef87d7adb83aacb83c8cc3352dcea9b0c54d
Alex Fischman <alexdfischman@gmail.com>
2024-03-15 00:13:58 -0700

    Change Program to &Program

--- brilift/src/lib.rs
+++ brilift/src/lib.rs
@@ -5,7 +5,7 @@ use crate::translator::{find_func, Translator};
 use bril_rs::Program;
 use cranelift_object::ObjectModule;
 
-pub fn compile(program: Program, target: Option<String>, opt_level: &str, output: &str) {
+pub fn compile(program: &Program, target: Option<String>, opt_level: &str, output: &str) {
     // Compile.
     let mut trans = Translator::<ObjectModule>::new(target, opt_level);
     trans.compile_prog(&program, false);
