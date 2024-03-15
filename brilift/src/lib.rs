b471388210acdcf22acb17a4871b2cbad61bd404
Alex Fischman <alexdfischman@gmail.com>
2024-03-14 17:13:24 -0700

    Update bril_rs Program import

--- brilift/src/lib.rs
+++ brilift/src/lib.rs
@@ -2,7 +2,7 @@ mod rt;
 pub mod translator;
 
 use crate::translator::{find_func, Translator};
-use bril_rs::program::Program;
+use bril_rs::Program;
 use cranelift_object::ObjectModule;
 
 pub fn compile(program: Program, target: Option<String>, opt_level: &str, output: &str) {
