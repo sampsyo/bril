f506b7e51bf54df4c2809c23fbc2118b40dab936
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2024-10-22 23:30:38 -0400

    Fixes https://github.com/sampsyo/bril/issues/342

--- bril-rs/brillvm/src/llvm.rs
+++ bril-rs/brillvm/src/llvm.rs
@@ -1294,7 +1294,7 @@ pub fn create_module_from_program<'a>(
                     }
                 });
 
-                (llvm_func, instrs, block, heap)
+                (llvm_func, instrs, block, heap, return_type)
             },
         )
         .collect(); // Important to collect, can't be done lazily because we need all functions to be loaded in before a call instruction of a function is processed.
@@ -1302,20 +1302,21 @@ pub fn create_module_from_program<'a>(
     // Now actually build each function
     funcs
         .into_iter()
-        .for_each(|(llvm_func, instrs, mut block, heap)| {
+        .for_each(|(llvm_func, instrs, mut block, heap, return_type)| {
             let mut last_instr = None;
+
+            // Maps labels to llvm blocks for jumps
+            let mut block_map = HashMap::new();
 
             // If their are actually instructions, proceed
             if !instrs.is_empty() {
                 builder.position_at_end(block);
 
-                // Maps labels to llvm blocks for jumps
-                let mut block_map = HashMap::new();
                 instrs.iter().for_each(|i| match i {
                     bril_rs::Code::Label { label, .. } => {
                         let new_block = block_map_get(context, llvm_func, &mut block_map, label);
 
-                        // Check if wee need to insert a jump since all llvm blocks must be terminated
+                        // Check if we need to insert a jump since all llvm blocks must be terminated
                         if !is_terminating_instr(&last_instr) {
                             builder
                                 .build_unconditional_branch(block_map_get(
@@ -1354,7 +1355,19 @@ pub fn create_module_from_program<'a>(
 
             // Make sure every function is terminated with a return if not already
             if !is_terminating_instr(&last_instr) {
-                builder.build_return(None).unwrap();
+                if return_type.is_none() {
+                    builder.build_return(None).unwrap();
+                } else {
+                    // This block did not have a terminating instruction
+                    // Returning void is ill-typed for this function
+                    // This code should be unreachable in well-formed Bril
+                    // Let's just arbitrarily jump to avoid needing to
+                    // instantiate a valid return value.
+                    assert!(!block_map.is_empty());
+                    builder
+                        .build_unconditional_branch(*block_map.values().next().unwrap())
+                        .unwrap();
+                }
             }
         });
 
