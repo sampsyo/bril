9f0ec0ea533d6eee895ce5b42adbb3a7a0743581
Adrian Sampson <adrian@radbox.org>
2025-02-20 12:34:42 -0500

    Format with ruff

--- examples/to_ssa_crude.py
+++ examples/to_ssa_crude.py
@@ -3,7 +3,6 @@ import sys
 
 from cfg import block_map, successors, add_terminators, add_entry, reassemble
 from form_blocks import form_blocks
-from xml.dom.expatbuilder import theDOMImplementation
 
 
 def get_types(func):
@@ -13,10 +12,10 @@ def get_types(func):
     type for every variable within a given function. So we just take the
     type of the first assignment we see to each variable in the function.
     """
-    types = {arg['name']: arg['type'] for arg in func.get('args', [])}
-    for instr in func['instrs']:
-        if 'dest' in instr:
-            types[instr['dest']] = instr['type']
+    types = {arg["name"]: arg["type"] for arg in func.get("args", [])}
+    for instr in func["instrs"]:
+        if "dest" in instr:
+            types[instr["dest"]] = instr["type"]
     return types
 
 
@@ -31,11 +30,15 @@ def block_to_ssa(block, block_name, succ_names, var_types):
     # Replace all variables with local names.
     version = {v: 0 for v in var_types}
     for instr in block:
-        if 'args' in instr:
-            instr['args'] = [local_name(block_name, a, version[a]) for a in instr['args']]
-        if 'dest' in instr:
-            version[instr['dest']] += 1
-            instr['dest'] = local_name(block_name, instr['dest'], version[instr['dest']])
+        if "args" in instr:
+            instr["args"] = [
+                local_name(block_name, a, version[a]) for a in instr["args"]
+            ]
+        if "dest" in instr:
+            version[instr["dest"]] += 1
+            instr["dest"] = local_name(
+                block_name, instr["dest"], version[instr["dest"]]
+            )
 
     # Add phis to the top.
     for var, type in var_types.items():
@@ -45,16 +48,19 @@ def block_to_ssa(block, block_name, succ_names, var_types):
     # Add upsilons to the bottom, before the terminator.
     for succ in succ_names:
         for var in var_types:
-            upsilon = {"op": "upsilon", "args": [
-                local_name(succ, var),
-                local_name(block_name, var, version[var]),
-            ]}
+            upsilon = {
+                "op": "upsilon",
+                "args": [
+                    local_name(succ, var),
+                    local_name(block_name, var, version[var]),
+                ],
+            }
             block.insert(-1, upsilon)
 
 
 def func_to_ssa(func):
     # Construct a well-behaved CFG.
-    blocks = block_map(form_blocks(func['instrs']))
+    blocks = block_map(form_blocks(func["instrs"]))
     add_entry(blocks)
     add_terminators(blocks)
     succ = {name: successors(block[-1]) for name, block in blocks.items()}
@@ -64,14 +70,14 @@ def func_to_ssa(func):
         block_to_ssa(block, name, succ[name], var_types)
 
     # Reassemble the CFG for output.
-    func['instrs'] = reassemble(blocks)
+    func["instrs"] = reassemble(blocks)
 
 
 def to_ssa(bril):
-    for func in bril['functions']:
+    for func in bril["functions"]:
         func_to_ssa(func)
     return bril
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(json.dumps(to_ssa(json.load(sys.stdin)), indent=2, sort_keys=True))
